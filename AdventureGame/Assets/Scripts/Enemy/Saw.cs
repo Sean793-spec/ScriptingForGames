e3214df6b49a38194ac01b9e74fd6bdaef03967e
Sean O'Hara <10890388@uvu.edu>
2024-11-22 15:48:06 -0700

    Unit 8 Challenge

--- AdventureGame/Assets/Scripts/Enemy/Saw.cs
+++ AdventureGame/Assets/Scripts/Enemy/Saw.cs
@@ -8,17 +8,29 @@ using UnityEngine.EventSystems;
 
 public class Saw : MonoBehaviour
 {
-    private Animator anim;
+    public Animator anim;
     public Health playerHealth;
     [SerializeField] private float damage;
 
-   private void OnTriggerEnter2D(Collider2D collision)
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     { 
         if (collision.tag == "Player")
         {
             playerHealth.TakeDamage(damage);
-            
+            anim.SetTrigger("Off");
+           StartCoroutine(AnimationTimer(3f));
         }
         
+    }
+
+    private IEnumerator AnimationTimer(float time)
+    {
+        yield return new WaitForSeconds(time);
+       anim.SetTrigger("On");
     }
 }
